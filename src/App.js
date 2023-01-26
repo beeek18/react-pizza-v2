73ca35fead052b9f41a8eb2951e632f9537c2f36
Bekzodbek <kodirov2000@list.ru>
2023-01-27 00:29:37 +0300

    add redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from './redux/slices/filterSlice'
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -8,19 +10,42 @@ import NotFound from "./pages/NotFound";
 
 import "./scss/app.scss";
 
+export const SearchContext = React.createContext()
+
 function App() {
   const [searchValue, setSearchValue] = React.useState('')
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
+
 
   return (
+
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <Routes>
-          <Route path="" element={<Home searchValue={searchValue} />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+
+        <Header />
+
+        <div className="content">
+          <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
