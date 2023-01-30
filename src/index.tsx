b85cf07023112c3baf261cdbe80497ded612a531
Bekzodbek <kodirov2000@list.ru>
2023-01-31 00:15:26 +0300

    rebuilding on TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -7,12 +6,16 @@ import App from "./App";
 
 import { store } from './redux/store'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElem = document.getElementById("root")
+
+if(rootElem){
+  const root = ReactDOM.createRoot(rootElem);
 
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <Provider store={ store } >
+  <App />
+  </Provider>
+</BrowserRouter>
 );
+}
