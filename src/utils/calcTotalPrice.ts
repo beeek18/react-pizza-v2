896fc04b4c6bcbbefed3d2a500a511e93a094896
Bekzodbek <kodirov2000@list.ru>
2023-02-04 01:07:58 +0300

    rebuilding

--- src/utils/calcTotalPrice.ts
+++ src/utils/calcTotalPrice.ts
@@ -1,7 +1,5 @@
-import { CartItem } from "../redux/cart/types";
+import { CartItem } from '../redux/cart/types';
 
 export const calcTotalPrice = (items: CartItem[]) => {
-  return items.reduce((sum, obj) => {
-    return (obj.price * obj.count) + sum;
-  }, 0);
-}
+  return items.reduce((sum, obj) => obj.price * obj.count + sum, 0);
+};
--- src/utils/getCartFromLS.ts
+++ src/utils/getCartFromLS.ts
@@ -1,14 +1,13 @@
-import { CartItem } from "../redux/cart/types";
-import { calcTotalPrice } from "./calcTotalPrice";
+import { CartItem } from '../redux/cart/types';
+import { calcTotalPrice } from './calcTotalPrice';
 
 export const getCartFromLS = () => {
-  const data = localStorage.getItem('cart')
+  const data = localStorage.getItem('cart');
   const items = data ? JSON.parse(data) : [];
   const totalPrice = calcTotalPrice(items);
 
   return {
     items: items as CartItem[],
     totalPrice,
-  }
-
-}
+  };
+};
